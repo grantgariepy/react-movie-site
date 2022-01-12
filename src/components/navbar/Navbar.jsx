8e90bc6d35c0ae5e3fb54eea31358d3e974d44fb
Grant <grantgariepy@protonmail.com>
2022-01-11 22:12:28 -0700

    navbar mostly done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons"
+import { useState } from "react"
 import "./navbar.scss"
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="container">
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
+
+    console.log(isScrolled);
+    return (    
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+            <div className="container"> 
                 <div className="left">
                     <img src="https://img.search.brave.com/GAWh4mkxAqNHCWfLj8fk1bdTbAlBGEfMVERXQuNzJpg/fit/1200/562/ce/1/aHR0cHM6Ly93d3cu/cGluY2xpcGFydC5j/b20vcGljZGlyL2Jp/Zy8yLTIzMjM2X3Ry/YW5zcGFyZW50LW5l/dGZsaXgtbG9nby0y/MDE4LWNsaXBhcnQu/cG5n"
                          alt="" />
@@ -15,11 +24,18 @@ const Navbar = () => {
                     <span>Popular</span>
                 </div>
                 <div className="right">
-                    <Search/>
+                    <Search className="icon"/>
                     <span>KID</span>
-                    <Notifications/>
+                    <Notifications className="icon"/>
                     <img src="https://i.imgur.com/0HVlygL.jpg" alt="" />
-                    <ArrowDropDown/>
+                    <div className="profile">
+                        <ArrowDropDown className="icon"/>
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,6 +6,115 @@ const Home = () => {
     return (
         <div className='home'>
             <Navbar/>
+            <img width="100%" src="https://i.imgur.com/0HVlygL.jpg" alt="" />
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
+
+            68,594 views • Jun 26, 2021 • How to create React Netflix App UI design for beginners. React UI Design course Netflix Clone. React movie website tutorial using hooks.
         </div>
     )
 }
