57a5cb4599e1cbb1daee6dbeca08403a963ff878
Grant <grantgariepy@protonmail.com>
2022-01-12 21:49:28 -0700

    list items started

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from "../components/featured/Featured";
+import List from "../components/list/List";
 import Navbar from "../components/navbar/Navbar";
 
 import "./home.scss";
@@ -9,6 +10,10 @@ const Home = () => {
         <div className='home'>
             <Navbar/>
             <Featured />
+            <List/>
+            <List/>
+            <List/>
+            <List/>
         </div>
     )
 }
