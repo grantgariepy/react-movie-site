33412d28f9f3efc70e78144536c3cd93178a65e8
Grant <grantgariepy@protonmail.com>
2022-01-12 22:28:34 -0700

    listItem icons

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,3 +1,4 @@
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@material-ui/icons"
 import "./listItem.scss"
 
 export default function ListItem() {
@@ -6,7 +7,10 @@ export default function ListItem() {
             <img src="https://img.search.brave.com/48D1_-AWJSFpNdSQGyBIQ8W8z-HAU2w6BvYIZAJKsLk/fit/1200/630/ce/1/aHR0cHM6Ly9zdGF0/aWMxLnNyY2RuLmNv/bS93b3JkcHJlc3Mv/d3AtY29udGVudC91/cGxvYWRzLzIwMTkv/MTAvVGhlLUxpZ2h0/aG91c2UtMjAxOS1i/YW5uZXIuanBn" alt="" />
             <div className="itemInfo">
                 <div className="icons">
-                    
+                    <PlayArrow/>
+                    <Add/>
+                    <ThumbUpAltOutlined/>
+                    <ThumbDownAltOutlined/>
                 </div>
             </div>
         </div>
